7a23868c096d132dc1a715547bca67ec1843afef
rlDiversion <dylan.vn@live.be>
2022-09-13 20:07:08 +0200

    delete unnecessary blockpos
    Signed-off-by: rlDiversion <dylan.vn@live.be>

--- src/main/java/com/rldiversion/collectomania/entity/EntityBug.java
+++ src/main/java/com/rldiversion/collectomania/entity/EntityBug.java
@@ -1,11 +1,9 @@
 package com.rldiversion.collectomania.entity;
 
 import net.minecraft.entity.passive.EntityAmbientCreature;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class EntityBug extends EntityAmbientCreature {
-    private BlockPos spawnPosition;
     public EntityBug(World worldIn)
     {
         super(worldIn);
